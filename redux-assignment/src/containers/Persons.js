23acd6a4946027650959f40f06d93da126554b8f
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-17 18:16:04 +0200

    combining Local UI State and Redux - added input fields in AddPerson and converted it to class based and setState

--- redux-assignment/src/containers/Persons.js
+++ redux-assignment/src/containers/Persons.js
@@ -31,7 +31,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return{
-        onAddPerson : () => dispatch({type : actionTypes.ADD_PERSON}),
+        onAddPerson : (name,age) => dispatch({type : actionTypes.ADD_PERSON , personData : {name:name, age:age}}),
         onDeletePerson : (id) => dispatch ({ type: actionTypes.DELETE_PERSON, personId :id})
     }
 }
